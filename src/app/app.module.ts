cf8aa92f0d1ae27cbd14d1201f28c64443c920dc
normanrahmel <92368615+normanrahmel@users.noreply.github.com>
2023-03-04 10:15:02 +0100

    adding Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MenuComponent } from './menu/menu.component';
+import { ContentComponent } from './content/content.component';
+import { Co2TableComponent } from './co2-table/co2-table.component';
+import { InputFieldsComponent } from './input-fields/input-fields.component';
+import { ResponsiveComponent } from './responsive/responsive.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuComponent,
+    ContentComponent,
+    Co2TableComponent,
+    InputFieldsComponent,
+    ResponsiveComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/co2-table/co2-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-co2-table',
+  templateUrl: './co2-table.component.html',
+  styleUrls: ['./co2-table.component.scss']
+})
+export class Co2TableComponent {
+
+}
