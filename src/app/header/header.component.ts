aa5a55908fd204ba0d6e8fbbbff492fef57858cf
normanrahmel <92368615+normanrahmel@users.noreply.github.com>
2023-03-13 08:26:15 +0100

    Die Webseite besitzt ein Menü mit lokalen Links, das je nach Schriftkultur der Besuchenden rechts oder links dargestellt wird.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,8 +3,49 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-
+  direction: string;
+  alignment: string;
+  reverseOrder: boolean;
+  /*
+  ngOnInit() {
+    const language = navigator.language.toLowerCase();
+    if (
+      language.startsWith('ab') ||
+      language.startsWith('fa') ||
+      language.startsWith('he')
+    ) {
+      this.direction = 'rtl';
+    } else {
+      this.direction = 'ltr';
+    }
+  }*/
+  /*
+  ngOnInit() {
+    const language = navigator.language.toLowerCase();
+    //if (language.startsWith('ar')) {
+    if (language.startsWith('de')) {
+      this.direction = 'rtl';
+      this.alignment = 'start';
+    } else {
+      this.direction = 'ltr';
+      this.alignment = 'end';
+    }
+  }*/
+  ngOnInit() {
+    const language = navigator.language.toLowerCase();
+    if (language.startsWith('ar')) {
+      //this.direction = 'rtl';
+      //this.alignment = 'start';
+      this.direction = 'ltr';
+      this.alignment = 'start';
+    } else {
+      //this.direction = 'ltr';
+      //this.alignment = 'start';
+      this.direction = 'rtl';
+      this.alignment = 'start';
+    }
+  }
 }
